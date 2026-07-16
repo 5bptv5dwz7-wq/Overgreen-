232f8c011bc0f6b1a4838ca310c2660cbde4ab9c
5bptv5dwz7-wq <5bptv5dwz7@privaterelay.appleid.com>
2026-07-16 15:14:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='overgreen-login-dropdown-v2-1';
+const CACHE='overgreen-v7';
 const FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

47d927fcaaf2b7a90aa1f642ffa5fc149a571e92
5bptv5dwz7-wq <5bptv5dwz7@privaterelay.appleid.com>
2026-07-17 16:34:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='overgreen-cloud-v27-scheda-punto-vendita';
+const CACHE='overgreen-cloud-v30-report-pdf';
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
